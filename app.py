e77f67fff666c0e5b0fa362bd1b74600c5e2f645
JSeptien <jseptienr@hotmail.com>
2015-11-13 16:23:39 -0800

    added stream and post view

--- app.py
+++ app.py
@@ -94,9 +94,26 @@ def post():
         return redirect(url_for('index'))
     return render_template('post.html', form=form)
 
+
+@app.route('/stream')
+@app.route('/stream/<username>')
+def stream(username=None):
+    template = 'stream.html'
+    if username and username != current_user.username:
+        user = models.User.select().where(models.User.username**username).get()
+        stream = user.posts.limit(100)
+    else:
+        stream = current_user.get_stream().limit(100)
+        user = current_user
+    if username:
+        template = 'user_stream.html'
+    return render_template(template, stream=stream)
+
+
 @app.route('/')
 def index():
-    return 'Hello World!'
+    stream = models.Post.select().limit(100)
+    return render_template('stream.html', stream=stream)
 
 
 if __name__ == '__main__':
